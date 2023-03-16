18c4d392b6c08c44452e38203c259a1db26f0704
Annie <anniehwang428@gmail.com>
2023-03-16 14:11:23 -0400

    adds reverse, sort, map and twitter

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,3 +1,5 @@
+import { Timeline } from "react-twitter-widgets";
+
 export function Content() {
   let student = {
     first_name: "Michael",
@@ -6,9 +8,56 @@ export function Content() {
     phone_number: "888-555-5556",
     short_bio: "I sell the best paper at the best paper company in the world.",
     linkedin_url: "https://www.linkedin/in/michaelscott",
+    twitter_handle: "TwitterDev",
     personal_blog_website_url: "test.com",
     github_url: "https://github.com/michaelscott",
+    experiences: [
+      {
+        id: 1,
+        start_date: "12/01/23",
+        end_date: "16/04/13",
+        job_title: "Taxi Driver",
+        company_name: "Relaxi-Cab",
+        details: "Drove a taxicab for mobile massages",
+      },
+      {
+        id: 2,
+        start_date: "17/05/04",
+        end_date: "19/12/12",
+        job_title: "Buyer",
+        company_name: "Ralph Lauren",
+        details: "Bought clothing for Ralph Lauren",
+      },
+      {
+        id: 3,
+        start_date: "22/08/25",
+        end_date: "present",
+        job_title: "Paleontologist",
+        company_name: "A University",
+        details: "do dino thangs and teach",
+      },
+    ],
+    educations: [
+      {
+        id: 1,
+        start_date: "04/04/04",
+        end_date: "08/08/08",
+        degree: "High School",
+        university_name: " Mizzou",
+        details: "The school is fluent in Japanese, and allows everyone to eat bagel bites.",
+      },
+      {
+        id: 1,
+        start_date: "05/05/05",
+        end_date: "10/10/10",
+        degree: "Bachelors",
+        university_name: "Louisiana State",
+        details: "The school is fluent in Japanese, and allows everyone to eat bagel bites.",
+      },
+    ],
   };
+  let experiences = student.experiences.reverse();
+  let educations = student.educations.reverse();
   return (
     <div>
       <link
@@ -18,16 +67,16 @@ export function Content() {
         media="all"
       />
       <link rel="stylesheet" type="text/css" href="resume.css" media="all" />
-      <div id="doc2" class="yui-t7">
+      <div id="doc2" className="yui-t7">
         <div id="inner">
           <div id="hd">
-            <div class="yui-gc">
-              <div class="yui-u first">
+            <div className="yui-gc">
+              <div className="yui-u first">
                 <h1>{`${student.first_name} ${student.last_name}`} </h1>
                 <h2>Web Developer, Software Engineer</h2>
               </div>
-              <div class="yui-u">
-                <div class="contact-info">
+              <div className="yui-u">
+                <div className="contact-info">
                   {/* <h3>
                     <a id="pdf" href="#">
                       Download PDF
@@ -52,29 +101,29 @@ export function Content() {
           </div>
           <div id="bd">
             <div id="yui-main">
-              <div class="yui-b">
-                <div class="yui-gf">
-                  <div class="yui-u first">
+              <div className="yui-b">
+                <div className="yui-gf">
+                  <div className="yui-u first">
                     <h2>Profile</h2>
                   </div>
-                  <div class="yui-u">
-                    <p class="enlarge">{`${student.short_bio}`}</p>
+                  <div className="yui-u">
+                    <p className="enlarge">{`${student.short_bio}`}</p>
                   </div>
                 </div>
-                <div class="yui-gf">
-                  <div class="yui-u first">
+                <div className="yui-gf">
+                  <div className="yui-u first">
                     <h2>Skills</h2>
                   </div>
-                  <div class="yui-u">
-                    <div class="talent">
+                  <div className="yui-u">
+                    <div className="talent">
                       <h2>Web Design</h2>
                       <p>Assertively exploit wireless initiatives rather than synergistic core competencies. </p>
                     </div>
-                    <div class="talent">
+                    <div className="talent">
                       <h2>Interface Design</h2>
                       <p>Credibly streamline mission-critical value with multifunctional functionalities. </p>
                     </div>
-                    <div class="talent">
+                    <div className="talent">
                       <h2>Project Direction</h2>
                       <p>
                         Proven ability to lead and manage a wide variety of design and development projects in team and
@@ -83,72 +132,91 @@ export function Content() {
                     </div>
                   </div>
                 </div>
-                <div class="yui-gf">
-                  <div class="yui-u first">
+                <div className="yui-gf">
+                  <div className="yui-u first">
                     <h2>Technical</h2>
                   </div>
-                  <div class="yui-u">
-                    <ul class="talent">
+                  <div className="yui-u">
+                    <ul className="talent">
                       <li>XHTML</li>
                       <li>CSS</li>
-                      <li class="last">Javascript</li>
+                      <li className="last">Javascript</li>
                     </ul>
-                    <ul class="talent">
+                    <ul className="talent">
                       <li>Jquery</li>
                       <li>PHP</li>
-                      <li class="last">CVS / Subversion</li>
+                      <li className="last">CVS / Subversion</li>
                     </ul>
-                    <ul class="talent">
+                    <ul className="talent">
                       <li>OS X</li>
                       <li>Windows XP/Vista</li>
-                      <li class="last">Linux</li>
+                      <li className="last">Linux</li>
                     </ul>
                   </div>
                 </div>
-                <div class="yui-gf">
-                  <div class="yui-u first">
+                <div className="yui-gf">
+                  <div className="yui-u first">
                     <h2>Experience</h2>
                   </div>
-                  <div class="yui-u">
-                    <div class="job">
-                      <h2>A University</h2>
-                      <h3>Paleontologist</h3>
-                      <h4>August 2022 - Present</h4>
-                      <p>Doing dino thangs and teach. </p>
-                    </div>
-                    <div class="job">
-                      <h2>Ralph Lauren</h2>
-                      <h3>Buyer</h3>
-                      <h4>May 2017 - December 2019</h4>
-                      <p>Bought clothing for Ralph Lauren. </p>
-                    </div>
-                    <div class="job">
-                      <h2>Relaxi-Cab</h2>
-                      <h3>Taxi Driver</h3>
-                      <h4>January 2012 - April 2016</h4>
-                      <p>Drove a cab for mobile massages </p>
-                    </div>
+                  <div className="yui-u">
+                    {/* <p> {testDate} </p> */}
+                    {/* {student.experiences.sort(function (a, b) {
+                      if (b.end_date === a.end_date) {
+                        return 0;
+                      } else if (b.end_date === "present") {
+                        return 1;
+                      } else if (a.end_date === "present") {
+                        return -1;
+                      } else {
+                        return new Date(b.end_date) - new Date(a.end_date);
+                      }
+                    })} */}
+                    {/* // remove reverse function // */}
+                    {experiences.map((experience) => {
+                      return (
+                        <div className="job" key={experience.id}>
+                          <h2>{`${experience.job_title}`}</h2>
+                          <h3>{`${experience.company_name}`}</h3>
+                          <h4>{`${experience.start_date} - ${experience.end_date}`} </h4>
+                          <p>{`${experience.details}`} </p>
+                        </div>
+                      );
+                    })}
                   </div>
                 </div>
-                <div class="yui-gf last">
-                  <div class="yui-u first">
+                <div className="yui-gf last">
+                  <div className="yui-u first">
                     <h2>Education</h2>
                   </div>
-                  <div class="yui-u">
-                    <h2>Louisiana State - Baton Rouge, LA </h2>
-                    <h3>
-                      BS: Computer Science &mdash; <strong>May 2005 - October 2010</strong>{" "}
-                    </h3>
-                    <h2>Mizzou High School- Townsend Hall Columbia, MO</h2>
-                    <h3>
-                      High School Diploma &mdash; <strong>April 2004 - August 2008</strong>{" "}
-                    </h3>
+                  <div className="yui-u">
+                    {educations.map((education) => {
+                      return (
+                        <div className="job" key={education.id}>
+                          <h2>
+                            {`${education.university_name}`} - {`${education.degree}`}
+                          </h2>
+                          <h4>{`${education.start_date} - ${education.end_date}`} </h4>
+                          <h5>{`${education.details}`}</h5>
+                        </div>
+                      );
+                    })}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div class="twitter-container">
+        <Timeline
+          dataSource={{
+            sourceType: "profile",
+            screenName: student.twitter_handle,
+          }}
+          options={{
+            height: "400",
+          }}
+        />
       </div>
     </div>
   );
